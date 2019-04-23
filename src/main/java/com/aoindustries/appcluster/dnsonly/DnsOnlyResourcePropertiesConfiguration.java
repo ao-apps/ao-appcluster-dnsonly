030ade768e66504fa3c50b8069cd28204dd3f618
AO Industries, Inc <support@aoindustries.com>
2019-04-23 03:46:37 -0500

    Minimum Java version changed from 1.6 to 1.7.

--- src/main/java/com/aoindustries/appcluster/dnsonly/DnsOnlyResourcePropertiesConfiguration.java
+++ src/main/java/com/aoindustries/appcluster/dnsonly/DnsOnlyResourcePropertiesConfiguration.java
@@ -55,7 +55,7 @@ public class DnsOnlyResourcePropertiesConfiguration extends ResourcePropertiesCo
 	public Set<? extends DnsOnlyResourceNodePropertiesConfiguration> getResourceNodeConfigurations() throws AppClusterConfigurationException {
 		String resourceId = getId();
 		Set<String> nodeIds = properties.getUniqueStrings("appcluster.resource."+id+".nodes", true);
-		Set<DnsOnlyResourceNodePropertiesConfiguration> resourceNodes = new LinkedHashSet<DnsOnlyResourceNodePropertiesConfiguration>(nodeIds.size()*4/3+1);
+		Set<DnsOnlyResourceNodePropertiesConfiguration> resourceNodes = new LinkedHashSet<>(nodeIds.size()*4/3+1);
 		for(String nodeId : nodeIds) {
 			if(!resourceNodes.add(new DnsOnlyResourceNodePropertiesConfiguration(properties, resourceId, nodeId, type))) throw new AssertionError();
 		}
